dc65474e5c8d4cd44f4e11f4f34cef3c5de6d4ce
Enol-Igareta <enol1code@gmail.com>
2023-02-22 20:52:31 +0100

    metodo post,get,delete

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,7 @@
 
 
-INSERT INTO anuncios_coche (id,asientos,autonomia,comunidad_autonoma,marca,nombre_cliente,precio,tipo) VALUES (default,2,200,"Asturias","Ferrari","Paco",500,"Deportivo")
+INSERT INTO anuncios_coche (id,asientos,autonomia,comunidad_autonoma,marca,nombre_cliente,precio,tipo) VALUES (default,2,200,"Asturias","Ferrari","Paco",50,"Deportivo")
+INSERT INTO anuncios_coche (id,asientos,autonomia,comunidad_autonoma,marca,nombre_cliente,precio,tipo) VALUES (default,4,500,"Andalucia","Golf","Pedro",100,"Mono volumen")
 
 
 
